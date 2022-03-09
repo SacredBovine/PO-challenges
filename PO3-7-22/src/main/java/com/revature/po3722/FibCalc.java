f40fe38165318d01fe5ae9279b987034fbf943df
Steven Hanley <Steven.Hanley@revature.net>
2022-03-09 09:06:38 -0600

    Week of 3/7/22 PO challenge.

--- PO3-7-22/src/main/java/com/revature/po3722/FibCalc.java
+++ PO3-7-22/src/main/java/com/revature/po3722/FibCalc.java
@@ -4,7 +4,7 @@ public class FibCalc {
 
     public static int calculateFib(int n){
         if (n > 1) return calculateFib(n-1) + calculateFib(n-2);
-        else if ( n == 1) return 1;
+        else if (n == 1) return 1;
         else return 0;
     }
 }
