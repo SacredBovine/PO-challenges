dc442c87bcf39623a5c7eb3792265d3b189badec
Steven Hanley <Steven.Hanley@revature.net>
2022-03-28 15:26:38 -0500

    It's the integer value square root of the number of lockers

--- PO3-28-22/src/com/revature/Main.java
+++ PO3-28-22/src/com/revature/Main.java
@@ -8,19 +8,20 @@ public class Main {
 
         final int numOfLockers = 144;
         boolean[] doors = new boolean[numOfLockers];
+
         for (int step = 1; step <= numOfLockers; step += 1){
             for (int locker = step - 1; locker < numOfLockers; locker += step){
                 doors[locker] = !doors[locker];
             }
         }
-        int i = 1;
+        int lockerNumber = 1;
         int count = 0;
         for(boolean out: doors){
             if(out) {
-                System.out.println(i);
+                System.out.println("Locker " + lockerNumber + " is open.");
                 count++;
             }
-            i++;
+            lockerNumber++;
         }
         System.out.println("There are " + count +" open lockers.");
         // the step between open doors increases by 2 each open door in the case that step += 1
@@ -28,6 +29,8 @@ public class Main {
         //      stepping by 2 gets all funky, maybe not generalizable
         //      stepping by 3 is even more funky.
         // Let's just generalize the step by 1 solution to arbitrary number of lockers.
+        // every locker # that is a perfect square is open.
+        // Will try (no promises) to find a series solution to explain
 
         // 100-120 -> 10 open lockers
         // 121-143 -> 11 open lockers
