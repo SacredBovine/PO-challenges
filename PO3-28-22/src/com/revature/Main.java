1f83766a625971e4a134c93abfadecb436824493
Steven Hanley <Steven.Hanley@revature.net>
2022-03-30 11:32:37 -0500

    It's the integer value square root of the number of lockers

--- PO3-28-22/src/com/revature/Main.java
+++ PO3-28-22/src/com/revature/Main.java
@@ -6,24 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Please set number of lockers to test with here
         final int numOfLockers = 100;
-       /* boolean[] doors = new boolean[numOfLockers];
 
-        /*for (int step = 1; step <= numOfLockers; step += 1){
+        boolean[] doors = new boolean[numOfLockers];
+
+        for (int step = 1; step <= numOfLockers; step += 1){
             for (int locker = step - 1; locker < numOfLockers; locker += step){
                 doors[locker] = !doors[locker];
             }
         }
-        int lockerNumber = 1;
+
         int count = 0;
-        for(boolean out: doors){
-            if(out) {
-                System.out.println("Locker " + lockerNumber + " is open.");
-                count++;
-            }
-            lockerNumber++;
-        }*/
-        System.out.println("There are " + (int) Math.sqrt(numOfLockers) +" open lockers.");
+        for(boolean out: doors) if(out) count++;
+
+        System.out.println();
+        System.out.println("case : Number of lockers = " + numOfLockers);
+        System.out.println("Programmatic solution: There are " + count + " open lockers.");
+        System.out.println("Mathematical solution: There are " + (int) Math.sqrt(numOfLockers) +" open lockers.");
+
         // the step between open doors increases by 2 each open door in the case that step += 1
         // Can this be generalized to step constraints?
         //      stepping by 2 gets all funky, maybe not generalizable
