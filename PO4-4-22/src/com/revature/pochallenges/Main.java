ea1e2ea7ce6ac436c0ffe8712facef351d584ec3
Steven Hanley <Steven.Hanley@revature.net>
2022-04-07 22:14:37 -0500

    Circular Array

--- PO4-4-22/src/com/revature/pochallenges/Main.java
+++ PO4-4-22/src/com/revature/pochallenges/Main.java
@@ -9,30 +9,29 @@ public class Main {
         String[] stringTest = {"a","b","c","d","e"};
         CircularArray<Integer> array1 = new CircularArray(intTest);
 
-        System.out.println("initial array: "+ array1.toString());
+        System.out.println("initial array: "+ array1);
         array1.rotateRight();
-        System.out.println("rotate right: " + array1.toString());
+        System.out.println("rotate right: " + array1);
         array1.rotateRight();
-        System.out.println("rotate right: " + array1.toString());
+        System.out.println("rotate right: " + array1);
 
         CircularArray<String> array2 = new CircularArray(stringTest);
-        System.out.println("initial array: "+ array2.toString());
+        System.out.println("initial array: "+ array2);
         array2.rotateLeft();
-        System.out.println("rotate left: " + array2.toString());
+        System.out.println("rotate left: " + array2);
         array2.rotateLeft();
-        System.out.println("rotate left: " + array2.toString());
+        System.out.println("rotate left: " + array2);
         System.out.println("get index 0: " + array2.get(0));
         System.out.println("get index 3: " + array2.get(3));
         System.out.println("get index 4: " + array2.get(4));
 
         array2.set(3, "foo");
-        System.out.println("Set index 3 to = \"foo\": " + array2.toString());
+        System.out.println("Set index 3 to = \"foo\": " + array2);
         System.out.println();
         System.out.println("demonstration of circular iteration:");
         for (int i = 0; i < 25; i++){
-            System.out.println(array2.next().toString());
+            System.out.println(array2.next());
         }
-
 
     }
 }
